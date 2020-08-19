330d7878f0deb7ffe331571ab468951662e51845
deurensis <66526039+deurensis@users.noreply.github.com>
2020-08-19 23:00:01 +0900

    Add files via upload

--- ma.js
+++ ma.js
@@ -1,7 +1,9 @@
 let gethp = document.getElementById('ohp');
+let yogethp = document.getElementById('y_ohp');
 let ma = document.getElementById('ma');
 let yo = document.getElementById('yo');
 let hpcount = 30;
+let yohpcount = 30;
 function changeImage() {
   if (hpcount <= 10) {
     ma.src = "kill.jpg";
@@ -11,6 +13,12 @@ function changeImage() {
     yo.src = "yongsa_win.jpg";
   }
 }
+function changeImage_ma_win() {
+    if (yohpcount <= 0) {
+      ma.src = 'ma_win.jpg';
+      yo.src = 'yongsa_kill.jpg'
+    }
+}
 function minus(attack) {
   if (hpcount > 0) {
     hpcount-=attack;
@@ -18,3 +26,10 @@ function minus(attack) {
   }
   changeImage();
 }
+setInterval(function() {
+  if (yohpcount > 0 && hpcount > 0) {
+    yohpcount-=3;
+    yogethp.innerHTML = yohpcount;
+  }
+changeImage_ma_win();
+},1000);
